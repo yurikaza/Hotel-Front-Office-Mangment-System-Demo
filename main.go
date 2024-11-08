0802886e1aba1fe582cb2c1ab48b3c2a7058b790
Yurikaza <yusufmirza55@hotmail.com>
2024-11-08 16:12:20 +0300

    Updated Auth System

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yurikaza/Hotel-Front-Office-Mangment-System-Demo-Backend/middleware"
+	"github.com/yurikaza/Hotel-Front-Office-Mangment-System-Demo-Backend/routes"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        port = "8000"
+    }
+
+    router := gin.New()
+    router.Use(gin.Logger())
+    routes.UserRoutes(router)
+
+    router.Use(middleware.Authentication())
+
+    // API-2
+    router.GET("/api-1", func(c *gin.Context) {
+
+        c.JSON(200, gin.H{"success": "Access granted for api-1"})
+
+    })
+
+    // API-1
+    router.GET("/api-2", func(c *gin.Context) {
+        c.JSON(200, gin.H{"success": "Access granted for api-2"})
+    })
+
+    router.Run(":" + port)
 }
